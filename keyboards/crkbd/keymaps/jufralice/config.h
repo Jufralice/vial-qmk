f4e38fd7c26237ba413d132ecdaae5d2cbffe278
France Julien <julien@xaop.com>
2026-03-18 19:43:39 +0100

    change the speed of the mouse accelations

--- keyboards/crkbd/keymaps/jufralice/config.h
+++ keyboards/crkbd/keymaps/jufralice/config.h
@@ -58,14 +58,14 @@
 // Constant mode:
 #define MK_3_SPEED
 #define MK_MOMENTARY_ACCEL
-#define MK_C_OFFSET_UNMOD 16
+#define MK_C_OFFSET_UNMOD 24
 #define MK_C_INTERVAL_UNMOD 16
 #define MK_C_OFFSET_0 4
 #define MK_C_INTERVAL_0 16
-#define MK_C_OFFSET_1 8
+#define MK_C_OFFSET_1 12
 #define MK_C_INTERVAL_1 16
-#define MK_C_OFFSET_2 32
-#define MK_C_INTERVAL_2 16
+#define MK_C_OFFSET_2 40
+#define MK_C_INTERVAL_2 8
 #define MK_W_OFFSET_UNMOD 1
 #define MK_W_INTERVAL_UNMOD 40
 #define MK_W_OFFSET_0 1
