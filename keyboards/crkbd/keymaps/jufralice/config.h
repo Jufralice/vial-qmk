8142648e6713e6c011f2e20bbeead8d346ef67af
France Julien <julien@xaop.com>
2026-03-24 09:51:01 +0100

    switch the normal mouse speed key with the slow one

--- keyboards/crkbd/keymaps/jufralice/config.h
+++ keyboards/crkbd/keymaps/jufralice/config.h
@@ -58,11 +58,11 @@
 // Constant mode:
 #define MK_3_SPEED
 #define MK_MOMENTARY_ACCEL
-#define MK_C_OFFSET_UNMOD 24
+#define MK_C_OFFSET_UNMOD 12
 #define MK_C_INTERVAL_UNMOD 16
 #define MK_C_OFFSET_0 4
 #define MK_C_INTERVAL_0 16
-#define MK_C_OFFSET_1 12
+#define MK_C_OFFSET_1 24
 #define MK_C_INTERVAL_1 16
 #define MK_C_OFFSET_2 40
 #define MK_C_INTERVAL_2 8
